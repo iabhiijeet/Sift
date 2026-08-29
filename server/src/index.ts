cb7c8e583e0f386c2024a3357853edd49bb83acf
iabhiijeet <abhijeetrao7800@gmail.com>
2026-08-29 19:27:33 +0530

    Implement user authentication models and routes; add database migration for user, session, account, and verification tables

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import { auth } from './lib/auth.js'
+import { toNodeHandler } from 'better-auth/node'
 
 const app = express()
 
 const port = 8080
+
+app.all('/api/auth/{*path}', toNodeHandler(auth))
 
 app.listen(port, ()=>{
     console.log(`App is listening on ${port}`);
--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -8,4 +8,10 @@ export const auth = betterAuth({
     }),
     baseURL: process.env.BETTER_AUTH_URL,
     secret: process.env.BETTER_AUTH_SECRET,
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+    },
 })
