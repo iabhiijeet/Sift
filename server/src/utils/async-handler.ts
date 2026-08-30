5fec893c0e5cadd068100108d2eb8d3a107cf5d9
iabhiijeet <abhijeetrao7800@gmail.com>
2026-08-30 20:53:57 +0530

    ui chnges

---
+++ server/src/utils/async-handler.ts
@@ -0,0 +1,19 @@
+import type {
+    Handler,
+    NextFunction,
+    Request,
+    RequestHandler,
+    Response,
+} from "express";
+
+type AsyncRequestHandler = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => Promise<void>;
+
+export function asynchandler(handler:AsyncRequestHandler):RequestHandler {
+    return (req,res,next)=>{
+        void handler(req,res,next).catch(next);
+    }
+}
---
+++ server/src/utils/workspace.validator.ts
@@ -0,0 +1,3 @@
+import {z} from "zod"
+
+export const CHAT_MODELS = ["gpt-4o-mini", "gpt-4o"
